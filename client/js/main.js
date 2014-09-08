69c27f63d4fa7d08649a06c958a64cb9c460eeb1
Mahdi Dibaiee <mdibaiee@aol.com>
2014-09-08 16:23:24 +0430

    Linted, fixed github link not working

--- client/js/main.js
+++ client/js/main.js
@@ -2,16 +2,16 @@ var $run = $('h1, h3, .options, a');
 
 $('li[contenteditable]').click(function(e) {
   if($(this).html() == 'عدد وارد کنید') $(this).html(' ');
-})
+});
 
 $('li[contenteditable]').on('keydown', function(e) {
   if(e.keyCode !== 8 && isNaN(+e.key)) return e.preventDefault();
-})
+});
 $('li[contenteditable]').on('keyup', function(e) {
   $(this).data('val', /\d*/.exec($(this).html()));
-})
+});
 
-$('a').click(function(e) {
+$('a[href="#"]').click(function(e) {
   e.preventDefault();
   var $this = $(this);
 
@@ -21,18 +21,18 @@ $('a').click(function(e) {
   $.ajax({
     url: url,
     success: success
-  })
-})
+  });
+});
 
 $('li').click(function() {
   $(this).parent().find('li').removeClass('active');
   $(this).addClass('active');
-})
+});
 
 function success(ipsum) {
   $('a').removeClass('running');
   $('.modal').html(ipsum);
- 
+
   var range = document.createRange();
   range.selectNodeContents($('.modal')[0]);
   var selection = window.getSelection();
